802f24a8fe4f7775c7b2c4d9a523383793a6516a
Chuks Eric-Mbah <chukwumaeric100@gmail.com>
2025-04-16 18:17:59 +0100

    Initial project setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container py-5">
+      <h1>Welcome to the Store</h1>
+    </div>
+  );
+};
+
+export default Home;
